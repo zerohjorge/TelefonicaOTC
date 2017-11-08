67ce73f2df879a8ea82f0504790dac8ae6224e93
Jorge Romero <jorge.romero.romero@gmail.com>
2017-11-08 18:52:22 -0300

    Add files via upload

---
+++ guestbook/guestbook.php
@@ -0,0 +1,31 @@
+<?php
+set_include_path('.:/usr/local/lib/php');
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require 'Predis/Autoloader.php';
+Predis\Autoloader::register();
+if (isset($_GET['cmd']) === true) {
+  $host = 'redis-master';
+  header('Content-Type: application/json');
+  if ($_GET['cmd'] == 'set') {
+    $client = new Predis\Client([
+      'scheme' => 'tcp',
+      'host'   => $host,
+      'port'   => 6379,
+    ]);
+    $client->set($_GET['key'], $_GET['value']);
+    print('{"message": "Updated"}');
+  } 
+else {
+    $host = 'redis-slave';
+    $client = new Predis\Client([
+      'scheme' => 'tcp',
+      'host'   => $host,
+      'port'   => 6379,
+    ]);
+    $value = $client->get($_GET['key']);
+    print('{"data": "' . $value . '"}');
+  }
+} else {
+  phpinfo();
+} ?>
